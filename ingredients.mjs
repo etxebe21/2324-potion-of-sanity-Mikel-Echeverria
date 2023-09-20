16c968394cf764e9555ebf712e66a2845046421f
Etxebe <mikelgartzia21@gmail.com>
2023-09-20 09:25:09 +0200

    [FIX] corrección, clase ingredient movida a nuevo fichero ingredient.mjs

--- ingredients.mjs
+++ ingredients.mjs
@@ -1,24 +1,6 @@
-import Effect from "./effects.mjs";
+import Ingredient from "./ingredient.mjs";
 
-class Ingredient
-{
-    constructor(name, effects, value, weight)
-    {
-        this.name = name;
-        this.effects = effects;
-        this.value = value;
-        this.weight = weight;
-    }
 
-    static from({name, effects, value, weight}) {
-        return new Ingredient(
-            name,
-            effects.map(effect => Effect.from(effect)),
-            value,
-            weight
-        );
-    }
-}
 
 class Ingredients
 {
